1b14b70200f4258c61bcf4448cbddb20c6718420
Velirax <veli.ivanovv@gmail.com>
2024-02-19 00:57:34 +0200

    flexbox change on coutries list component

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -1,17 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { Input } from '@angular/core';
-
+import { Component, EventEmitter } from '@angular/core';
+import { Input, Output } from '@angular/core';
+import { Country } from '../fetch-countries.service';
 @Component({
-  selector: 'app-country',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './country.component.html',
-  styleUrl: './country.component.css'
+    selector: 'app-country',
+    standalone: true,
+    templateUrl: './country.component.html',
+    styleUrl: './country.component.css',
+    imports: [CommonModule]
 })
 export class CountryComponent {
-@Input() name:Object = [];
-@Input() capital: string = '';
-@Input() flagUrl: string = '';
-@Input() population: string = '';
+    @Input() name:Object = [];
+    @Input() capital: string = '';
+    @Input() flagUrl: string = '';
+    @Input() population: string = '';
+    @Input()countryObject!: Country;
 }
